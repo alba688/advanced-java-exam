7a43ed4bb1f27ad9e735711827e60d0bdccbd1cb
Sigurd Ovesen <sigurd.ovesen@gmail.com>
2021-10-31 20:26:41 +0100

    Shouldreturn200OK ð Now reads from response. Fixes #2

--- src/main/java/no/kristiania/Eksamen/HttpClient.java
+++ src/main/java/no/kristiania/Eksamen/HttpClient.java
@@ -1,22 +1,37 @@
 package no.kristiania.Eksamen;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class HttpClient {
-    private String host;
-    private int port;
-    private String requestTarget;
+    private final int statusCode;
 
-    public HttpClient(String host, int port, String requestTarget ) {
+    public HttpClient(String host, int port, String requestTarget ) throws IOException {
 
-        this.host = host;
-        this.port = port;
-        this.requestTarget = requestTarget;
-    }
+            Socket socket = new Socket(host, port);
+            String request = "GET " + requestTarget + " HTTP/1.1\r\n" +
+                    "Host: " + host + "\r\n\r\n";
+            socket.getOutputStream().write(request.getBytes());
 
-    public int getStatusCode() {
-        return 200;
-    }
+            String statusLine = readLine(socket);
+            this.statusCode = Integer.parseInt(statusLine.split(" ")[1]);
+        }
 
-    public String getResponseHeader(String s) {
-        return " ";
-    }
-}
+        private String readLine (Socket socket) throws IOException {
+            StringBuilder line = new StringBuilder();
+            int c;
+            while ((c = socket.getInputStream().read()) != -1 && c != '\r') {
+                line.append((char) c);
+            }
+            socket.getInputStream().read();
+            return line.toString();
+        }
+
+            public int getStatusCode () {
+                return statusCode;
+            }
+
+            public String getResponseHeader (String s){
+                return " ";
+            }
+        };
--- src/test/java/no/kristiania/Eksamen/HttpClientTest.java
+++ src/test/java/no/kristiania/Eksamen/HttpClientTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class HttpClientTest {
 
     @Test
-    void shouldReturn200StatusCode() {
+    void shouldReturn200StatusCode() throws IOException {
         assertEquals(200, new HttpClient("httpbin.org", 80, "/html").getStatusCode());
     }
 
